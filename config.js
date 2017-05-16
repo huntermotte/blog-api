dda48f8ad561d7e01cf23ea5ee0a5799f4813c8b
Hunter Motte <huntermotte@gmail.com>
2017-05-15 20:17:47 -0400

    commit

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/blog-api-database';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin@ds137261.mlab.com:37261/blog-api-database';
 
 exports.PORT = process.env.PORT || 8080;
